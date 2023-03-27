337f0059288cb41a1dbe753d6b6f438ee17060b0
Unai Martinez-Corral <unai.martinezcorral@ehu.eus>
2023-03-27 22:11:11 +0200

    update from Godot 3.x to 4.x

--- container/gexport.sh
+++ container/gexport.sh
@@ -5,10 +5,10 @@
 
 set -e
 
-local_templates=~/.local/share/godot/templates
+local_templates=~/.local/share/godot/export_templates
 mkdir -p "$local_templates"
 ln -s \
-  /usr/local/share/godot/templates/"${GODOT_VERSION}.stable" \
+  /usr/local/share/godot/export_templates/"${GODOT_VERSION}.stable" \
   $local_templates/"${GODOT_VERSION}.stable"
 
-godot "$@"
+godot --headless "$@"
